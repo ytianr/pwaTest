5b917f69600f3ab387ea1561cdfe669eaf091b18
tian yang <1076821345@qq.com>
2019-08-31 22:52:42 +0800

    按掘金上教程改了下

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,7 +1,43 @@
-'use strict';
-importScripts('sw-toolbox.js');
-toolbox.precache(['index.html', 'style/style.css']);
-toolbox.router.get('/images/*', toolbox.cacheFirst);
-toolbox.router.get('/*', toolbox.networkFirst, {
-	networkTimeoutSeconds: 5
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open('v1').then(cache => {
+            return cache.addAll([
+                '/js/index.js',
+                '/index.html',
+                '/'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function (response) {
+            // 检测是否已经缓存过
+            if (response) {
+                return response;
+            }
+
+            var fetchRequest = event.request.clone();
+
+            return fetch(fetchRequest).then(
+                function (response) {
+                    // 检测请求是否有效
+                    if (!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    var responseToCache = response.clone();
+
+                    caches.open('v1')
+                        .then(function (cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                }
+            );
+        })
+    );
 });
